11426205ee11e9ab19b456141dc766cc8c907748
baymen1524 <baymen1524@gmail.com>
2024-05-25 00:01:00 +0300

    n

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const connectToDatabase = require("./database/connect");
 const cors = require("./middlewares/cors.js");
 
 const app = express();
-const PORT = 3000;
+const PORT = 3005;
 
 connectToDatabase();
 
